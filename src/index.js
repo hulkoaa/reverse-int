76c9ea06a8e39605e00bd0a51c0823b51ddabe12
Aliaksandr Hulko <hulkoaa@gmail.com>
2020-03-03 21:19:07 +0300

    init: add solution to reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+    let arr = [];
+    let str = n.toString();
+    for(i=0;i<str.length;i++){
+        if(str[i]!='-'){arr.push(str[i]);}
+    }
+    return arr.reverse().join('');
+
+
   
 }
